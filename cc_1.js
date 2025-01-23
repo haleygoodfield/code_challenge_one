572d41300330f66b5738dd405059f9229ffca94e
Haley Goodfield <haleygoodfield@usf.edu>
2025-01-22 19:19:48 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,15 @@
+// Task 1 - Variables
+//Declare a variable employeeName using let and assign it the employee's name as a string
+let employeeName = "Paris Hills";
+// Declare a variable employeeID using const and assign it the employee's ID as a number
+const employeeID = 777;
+// Declare a variable isActive using var and assign it a boolean value true to indicate the employee is currently active
+var inActive = true;
+
+// Log each variable's value and its type to the console using console.log() and typeof
+console.log("Employee Name", employeeName, "-", typeof employeeName);
+// Log each variable's value and its type to the console using console.log() and typeof
+console.log("Employee ID:", employeeID, "-", typeof employeeID);
+// Log each variable's value and its type to the console using console.log() and typeof
+console.log("Active:", isActive, "-", typeof isActive);
+
