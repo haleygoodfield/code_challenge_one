fec08e139ba3399d492ba0d82808823a80943161
Haley Goodfield <haleygoodfield@usf.edu>
2025-01-23 09:29:05 -0500

    Task 5 - Acess Control

--- cc_1.js
+++ cc_1.js
@@ -9,9 +9,9 @@ var isActive = true;
 // Log each variable's value and its type to the console using console.log() and typeof
 console.log("Employee Name:", employeeName, "-", typeof employeeName);
 // Log each variable's value and its type to the console using console.log() and typeof
-console.log("Employee ID:", employeeID, "-", typeof employeeID);
+console.log("Employee ID Number:", employeeID, "-", typeof employeeID);
 // Log each variable's value and its type to the console using console.log() and typeof
-console.log("Active:", isActive, "-", typeof isActive);
+console.log("Employee Is Active:", isActive, "-", typeof isActive);
 
 
 // Task 2 - Primive Data Tasks 
@@ -25,33 +25,33 @@ var isAvailable = true;
 // Log each variable's value and its type to the console
 console.log("Product Name:", productName, "-", typeof productName);
 // Log each variable's value and its type to the console.
-console.log("Product Price:", producePrice, "-", typeof producePrice);
+console.log("Product Price $:", producePrice, "-", typeof producePrice);
 // Log each variable's value and its type to the console.
-console.log("Is Available:", isAvailable, "-", typeof isAvailable);
+console.log("Product Is Available:", isAvailable, "-", typeof isAvailable);
 
 
 // Task 3 - Number Data Type 
 //Declare a variable accountBalance using let and assign it a number representing the initial balance
 let accountBalance = 5000;
- console.log("Initial Balance: $", accountBalance);
+ console.log("Initial Account Balance: $", accountBalance);
 
  // Perform basic arithmetic operations (+, -, *, /) and log the updated values
 
  // Addition
 accountBalance += 2000;
-console.log("Deposite $2000 Balance: $", accountBalance)
+console.log("Account Balance with $2000 Deposit: $", accountBalance)
 
 // Subtraction
 accountBalance -= 650
-console.log("Withdrawl $650 Balance: $", accountBalance)
+console.log("Account Balance with $650 Withdrawl: $", accountBalance)
 
 // Multiplication 
 accountBalance *= 0.75
-console.log("After Taxes Balance: $", accountBalance)
+console.log("Account Balance After Taxes: $", accountBalance)
 
 // Division
 accountBalance /= 2
-console.log("Taking Out Half: $", accountBalance)
+console.log("Account Balance After Taking Out Half: $", accountBalance)
 
 // Task 4 - String Data Type
 // Declare a variable customerName using let and assign it a string value representing the customer's name.
@@ -61,6 +61,16 @@ let customerName = "London Brown";
 let welcomeMessage = "We are pleased to welcome you " + customerName + ", thank you for choosing us!";
 console.log(welcomeMessage);
 
+
+// Task 5 - Boolean Data Type
+// Declare a variable isLoggedIn using let and assign it a boolean value indicating user login status.
+let isLoggedIn= true;
+
+// Use logical operations to check access conditions and log results to the console.
+if (isLoggedIn) {console.log ("Permission Granted: User is logged in.");} else {console.log("Permission DENIED: User is not logged in.");}
+
+// JavaScript Basic Coding Challenge is Complete!!!!!! :)
+ 
 
 
 
