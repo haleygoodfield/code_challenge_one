d3169bec8e5e1df5a37e22695e3f3191187e8525
Haley Goodfield <haleygoodfield@usf.edu>
2025-01-23 09:13:51 -0500

    Task 4 - Customer Messaging

--- cc_1.js
+++ cc_1.js
@@ -7,7 +7,7 @@ const employeeID = 777;
 var isActive = true;
 
 // Log each variable's value and its type to the console using console.log() and typeof
-console.log("Employee Name", employeeName, "-", typeof employeeName);
+console.log("Employee Name:", employeeName, "-", typeof employeeName);
 // Log each variable's value and its type to the console using console.log() and typeof
 console.log("Employee ID:", employeeID, "-", typeof employeeID);
 // Log each variable's value and its type to the console using console.log() and typeof
@@ -33,25 +33,37 @@ console.log("Is Available:", isAvailable, "-", typeof isAvailable);
 // Task 3 - Number Data Type 
 //Declare a variable accountBalance using let and assign it a number representing the initial balance
 let accountBalance = 5000;
- console.log("Initial Balance $", accountBalance);
+ console.log("Initial Balance: $", accountBalance);
 
  // Perform basic arithmetic operations (+, -, *, /) and log the updated values
 
  // Addition
 accountBalance += 2000;
-console.log("Input Balance: $", accountBalance)
+console.log("Deposite $2000 Balance: $", accountBalance)
 
 // Subtraction
 accountBalance -= 650
-console.log("Withdrawl Balance: $", accountBalance)
+console.log("Withdrawl $650 Balance: $", accountBalance)
 
 // Multiplication 
-accountBalance *= 5.5
-console.log("Taxes Added Balance: $", accountBalance)
+accountBalance *= 0.75
+console.log("After Taxes Balance: $", accountBalance)
 
 // Division
 accountBalance /= 2
 console.log("Taking Out Half: $", accountBalance)
+
+// Task 4 - String Data Type
+// Declare a variable customerName using let and assign it a string value representing the customer's name.
+let customerName = "London Brown";
+
+// Concatenate strings to form a welcome message and log it to the console
+let welcomeMessage = "We are pleased to welcome you " + customerName + ", thank you for choosing us!";
+console.log(welcomeMessage);
+
+
+
+
 
 
 
